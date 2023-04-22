7d28e978bb231fffdb5d4883d14c66a2761a8dfa
ThiagoDavid <88092348+ThiagoDavid@users.noreply.github.com>
2023-04-22 17:21:39 -0300

    Coletando todas as páginas pelo sitemap

--- myproject/myproject/spiders/NoticiasSpider.py
+++ myproject/myproject/spiders/NoticiasSpider.py
@@ -1,24 +1,44 @@
+# vou tentar atravez do sitemap do site extrair todas as notícias
 import scrapy
 from myproject.items import Noticia
-
+import xml.etree.ElementTree as ET
 
 class NoticiasSpider(scrapy.Spider):
-    name = "noticias"
-    allowed_domains = ["quatrorodas.abril.com.br"]
-    start_urls = ["https://quatrorodas.abril.com.br/noticias/"]
-
+    name = 'noticias'
+    allowed_domains = ['quatrorodas.abril.com.br']
+    start_urls = ['https://quatrorodas.abril.com.br/sitemap.xml']
+    custom_settings = {
+        'DOWNLOAD_DELAY': 0.5,        
+        'ROBOTSTXT_OBEY': True
+    }
+    numPaginas = 0
     def parse(self, response):
-        for noticia in response.css('div.card'):
-            item = Noticia()
-            item['titulo'] = noticia.css('h2.title, h3.title::text').get()  #tag h2 ou h3; classe title texto (inner text)
-            item['categoria'] = noticia.css('span.category::text').get()
-            item['img_alternative'] = noticia.css('img::attr(alt)').get()
-            item['image_link'] = noticia.css('img::attr(data-src)').get()
-            item['link'] = noticia.css('a::attr(href)').get() #tag a; atribuot href
-            item['descricao'] = noticia.css('p.description::text').get()
-            yield item
-        
-        #Paginacao
-        proxima_pagina = response.css('a.proxima::attr(href)').get()
-        if proxima_pagina is not None:
-            yield response.follow(proxima_pagina, callback = self.parse)
+        root = ET.fromstring(response.body)
+        count = 0
+        for child in root.iter('{http://www.sitemaps.org/schemas/sitemap/0.9}loc'):
+            if(count <= 2):
+                url = child.text
+                yield scrapy.Request(url, callback=self.parse_url_noticia)
+                count+=1
+
+    def parse_url_noticia(self, response):
+        root = ET.fromstring(response.body)
+        count=0
+        for child in root.iter('{http://www.sitemaps.org/schemas/sitemap/0.9}loc'):
+            if(count<=1):
+                url = child.text
+                yield scrapy.Request(url, callback=self.parse_noticia)
+                count+=1   
+
+    def parse_noticia(self, response):
+        item = Noticia()
+        item['titulo'] = response.css('h1.title::text').get()  #tag h2 ou h3; classe title texto (inner text)
+        item['descricao'] = response.css('h2.description::text').get()
+        item['conteudo'] = response.css('section.content').get(),
+        item['url'] = response.url
+        self.numPaginas += 1        
+        yield item
+
+    def closed(self, reason):
+        self.logger.info(f"Coletadas {self.numPaginas} páginas") 
+       
